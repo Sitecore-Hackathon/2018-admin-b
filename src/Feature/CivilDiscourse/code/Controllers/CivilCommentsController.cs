38cf120b5ce52613599a77c55a238475d06f4a0e
Ed Schwehm <ed@edschwehm.com>
2018-03-03 13:11:16 -0500

    Updates to MVC project in powerful ways

--- src/Feature/CivilDiscourse/code/Controllers/CivilCommentsController.cs
+++ src/Feature/CivilDiscourse/code/Controllers/CivilCommentsController.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
-using AdminB.Feature.CivilDiscourse.layouts;
 using AdminB.Feature.CivilDiscourse.Models;
 using Sitecore.Data;
 using Sitecore.Data.Fields;
 using Sitecore.Data.Items;
-using Website.Models;
 
 namespace AdminB.Feature.CivilDiscourse.Controllers
 {
@@ -127,6 +126,64 @@ namespace AdminB.Feature.CivilDiscourse.Controllers
             var severityItem = severity.TargetItem;
             if (severityItem == null) return "";
             return severityItem.Fields["Warning Color"].Value;
+        }
+    }
+
+    public class Word
+    {
+        public string Value { get; set; }
+        public string Warning { get; set; }
+        public string Color { get; set; }
+    }
+
+    public static class StringExtension
+    {
+        public static string SanitizeToItemName(this string possibleName)
+        {
+            return SanitizeToItemName(possibleName, Sitecore.Configuration.Settings.InvalidItemNameChars);
+        }
+
+        public static string SanitizeToItemName(this string possibleName, char[] invalidCharacters)
+        {
+            return string.Concat(possibleName.Trim().Split(invalidCharacters));
+        }
+        public static int CountMatches(this string source, string searchText)
+        {
+
+            if (string.IsNullOrWhiteSpace(source) || string.IsNullOrWhiteSpace(searchText))
+                return 0;
+
+            source = source.ToLower();
+            searchText = searchText.ToLower();
+
+            int counter = 0;
+            int startIndex = -1;
+            while ((startIndex = (source.IndexOf(searchText, startIndex + 1))) != -1)
+                counter++;
+            return counter;
+        }
+
+        public static string CapitalizeFirst(this string s)
+        {
+            bool IsNewSentense = true;
+            var result = new StringBuilder(s.Length);
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (IsNewSentense && char.IsLetter(s[i]))
+                {
+                    result.Append(char.ToUpper(s[i]));
+                    IsNewSentense = false;
+                }
+                else
+                    result.Append(s[i]);
+
+                if (s[i] == '!' || s[i] == '?' || s[i] == '.')
+                {
+                    IsNewSentense = true;
+                }
+            }
+
+            return result.ToString();
         }
     }
 }
--- src/Feature/CivilDiscourse/code/xConnect/Contact.cs
+++ src/Feature/CivilDiscourse/code/xConnect/Contact.cs
@@ -76,6 +76,5 @@ namespace CivilDiscorse.TextAnalyzer.xConnect
                 }
             }
         }
-        }
     }
 }
