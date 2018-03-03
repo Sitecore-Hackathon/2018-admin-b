029b2dd6c4c77d04d1558a8b3c12723fd6875252
Ed Schwehm <ed@edschwehm.com>
2018-03-03 01:46:59 -0500

    Adding base analyzer code; updating path to no-spaces; Adding analyzer code to solution

---
+++ src/Feature/CivilDiscourse/code/Analyzer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CivilDiscorse.TextAnalyzer
+{
+    public class Analyzer
+    {
+        protected List<string> _phraseList;
+        protected string _text;
+        
+        public Dictionary<string, int> Hits { get; protected set; }
+        public Analyzer(List<string> phraseList, string text)
+        {
+            _phraseList = phraseList;
+            _text = text;
+        }
+
+        public Dictionary<string, int> FindPhraseCountUsingLinq()
+        {
+            var results = new Dictionary<string, int>();
+
+            /*
+             *var filewords = Directory.GetFiles(@"H:\\backstage\my work\categories file text\categories", "*.txt")    
+        .Select(f => File.ReadAllText(f))                                                               // read all text in each file
+        .SelectMany(s => words.Intersect(Regex.Split(s, @"\W|_")))                                      // intersect the set of words in each file with the master list of words, then flatten the list
+        .GroupBy(s => s).ToDictionary(w => w.Key, w => w.Count());   
+             *
+             */
+
+            //var r1 = _text.SelectMany(s => )
+
+            return results;
+        }
+
+        public void FindPhrasesUsingRegex()
+        {
+            var results = new Dictionary<string, int>();
+
+            string pattern = @"^.*\b(" + string.Join("|", _phraseList) + @")\b.*$";
+
+            var matches = Regex.Matches(_text, pattern, RegexOptions.Multiline);
+            foreach (Match match in matches)
+            {
+                string theBadWord = match.Groups[1].Value;
+                if (results.ContainsKey(theBadWord))
+                {
+                    results[theBadWord]++;
+                }
+                else
+                {
+                    results.Add(theBadWord, 1);
+                }
+            }
+
+            Hits = results;
+        }
+    }
+}
---
+++ src/Feature/CivilDiscourse/tests/AnalyzerTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CivilDiscorse.TextAnalyzer.Tests
+{
+    [TestFixture]
+    public class AnalyzerTests
+    {
+        [Test]
+        public void Analyzer_OneLineOfTextWithOneOffendingWord_WordIsDetectedOnce()
+        {
+            var text = @"Communication is not possible. The shuttle has no power. We're fucked up the butt. Using the gravitational pull of a star to slingshot back in time? We are going to Starbase Montgomery for Engineering consultations prompted by minor read-out anomalies. Probes have recorded unusual levels of geological activity in all five planetary systems. Assemble a team. Look at records of the Drema quadrant. Would these scans detect artificial transmissions as well as natural signals?";
+            var phraseList = new List<string>() {"fuck", "shit", "butt"};
+
+            var theBadWord = "butt";
+
+            var textAnalyzer = new Analyzer(phraseList, text);
+
+            textAnalyzer.FindPhrasesUsingRegex();
+
+            Assert.IsNotNull(textAnalyzer.Hits);
+            Assert.IsNotNull(textAnalyzer.Hits[theBadWord]);
+            Assert.AreEqual(1, textAnalyzer.Hits[theBadWord]);
+        }
+
+        [Test]
+        public void Analyzer_ManyLineOfTextWithOneOffendingWord_WordIsDetectedOnce()
+        {
+            var text = 
+@"Communication is not possible. 
+The shuttle has no power. We're fucked up the butt. 
+Using the gravitational pull of a star to 
+slingshot back in time? We are going to 
+Starbase Montgomery for Engineering 
+consultations prompted by minor read-out 
+anomalies. Probes have recorded unusual 
+levels of geological activity in all five 
+planetary systems. Assemble a team. Look at 
+records of the Drema quadrant. Would these scans detect 
+artificial transmissions as well as natural signals?";
+            var phraseList = new List<string>() { "fuck", "shit", "butt" };
+
+            var theBadWord = "butt";
+
+            var textAnalyzer = new Analyzer(phraseList, text);
+
+            textAnalyzer.FindPhrasesUsingRegex();
+
+            Assert.IsNotNull(textAnalyzer.Hits);
+            Assert.IsNotNull(textAnalyzer.Hits[theBadWord]);
+            Assert.AreEqual(1, textAnalyzer.Hits[theBadWord]);
+        }
+
+        [Test]
+        public void Analyzer_ManyLineOfTextWithManyOffendingWords_AllBadWordsDetectedTheRightNumberOfTimes()
+        {
+            var text =
+@"Communication is not possible. 
+The shuttle has no power. We're fucked up the butt. 
+Using the gravitational pull of a star to 
+slingshot back in time? We are going to 
+Starbase Montgomery for shit Engineering 
+consultations prompted by minor butt read-out 
+anomalies. Probes have fuck recorded unusual 
+levels of geological activity in all five 
+planetary systems. Assemble a butt team. Look at 
+records of the Drema quadrant. Would these shit scans detect 
+artificial transmissions as well as fuck natural signals?";
+            var phraseList = new List<string>() { "fuck", "shit", "butt" };
+
+            var textAnalyzer = new Analyzer(phraseList, text);
+
+            textAnalyzer.FindPhrasesUsingRegex();
+
+            Assert.IsNotNull(textAnalyzer.Hits);
+            Assert.IsNotNull(textAnalyzer.Hits["butt"]);
+            Assert.AreEqual(3, textAnalyzer.Hits["butt"]);
+
+            Assert.IsNotNull(textAnalyzer.Hits["fuck"]);
+            Assert.AreEqual(2, textAnalyzer.Hits["fuck"]);
+
+            Assert.IsNotNull(textAnalyzer.Hits["shit"]);
+            Assert.AreEqual(2, textAnalyzer.Hits["shit"]);
+        }
+    }
+}
